e8e17d8c2980f2abf613a13092e48a2f74fbfef5
tuxunpro@gmail.com <root@localhost.localdomain>
2014-10-20 15:09:35 +0000

    O.3b

--- gitscr.sh
+++ gitscr.sh
@@ -1,3 +1,3 @@
-git config --global user.name "tuxun"
-git config --global user.name "tuxunpro@gmail.com"
+git config --global user.name 'tuxun'
+git config --global user.name 'tuxunpro@gmail.com'
 #git remote add origin https://github.com/tuxun/mwlab.git
